af7638f9811fabb153cb789d097bbe0d2296f5ff
Gaurav Rai Mazra <gaurav.r.mazra@gmail.com>
2020-06-24 19:31:17 -0400

    Update in urls

--- src/components/BlogRedirectInfo.tsx
+++ src/components/BlogRedirectInfo.tsx
@@ -15,13 +15,13 @@ interface IProps {
    }
    function newPostUrl(paths: string[]): string {
      if (isValidPostUrl(paths)) {
-       return `https://codefoundry.dev/${paths.join('/')}`;
+       return `https://www.codefoundry.dev/${paths.join('/')}`;
      }
-     return `https://codefoundry.dev/search?q=${paths.join(',')}&max-results=20&by-date=true`
+     return `https://www.codefoundry.dev/search?q=${paths.join(',')}&max-results=20&by-date=true`
    }
  
    function newSearch(keyword: string) {
-     return `https://codefoundry.dev/search?q=${keyword}&max-results=20&by-date=true`
+     return `https://www.codefoundry.dev/search?q=${keyword}&max-results=20&by-date=true`
    }
    const url = isString(prop.path) ? newSearch(prop.path as string) : newPostUrl(prop.path as Array<string>);
    
@@ -31,7 +31,7 @@ interface IProps {
          <div className={styles['big-box']}>
            <h1 className={styles['big-box-title']}>Hello Reader!</h1>
            <p className={styles['big-box-p']}>We are making changes to our site!</p>
-           <p className={styles['big-box-p']}><code className={styles['code']}>https://gauravbytes.com</code> is moving to <a className={styles['big-box-a']} href="https://codefoundry.dev"><code className={styles['code']}>https://codefoundry.dev</code></a></p>
+           <p className={styles['big-box-p']}><code className={styles['code']}>https://gauravbytes.com</code> is moving to <a className={styles['big-box-a']} href="https://www.codefoundry.dev"><code className={styles['code']}>https://codefoundry.dev</code></a></p>
            {!!prop.path && <hr className={styles['big-box-hr']} />}
            {!!prop.path && <p>The new url to see this post is: <a className={styles['big-box-a']} href={url}><code className={styles['code-r']}>{url}</code></a></p>}
          </div>
